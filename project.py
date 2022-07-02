e851a231e90dd0fade0bd553a651e57bb45dfcfd
Andrew Rednikov <andrewrednikov@outlook.com>
2022-07-02 19:11:13 -0400

    Built dictionaries with random order to act as databases

--- project.py
+++ project.py
@@ -1,10 +1,29 @@
+import random
 import numpy as np
 import pandas as pd
 
-def buildDict(excelName: str):
+def buildDatabases(excelName: str) -> list:
     df = pd.read_excel(excelName, usecols=[0,1])
+    
+    #create dictionary of the partners with their assigned dances
     dancesByPartner = df.groupby(["Partners"])["Dances"].apply(list).to_dict()
     
+    #create dictionary of the dances in a random order
+    allDances = df["Dances"].unique().tolist()
+    randDanceOrder = random.sample(allDances, k = len(allDances))
+    danceOrder = {key:value for key,value in enumerate(randDanceOrder)}
+
+    #create a dictionary of the partners in a random order
+    allPartners = df["Partners"].unique().tolist()
+    randPartnerOrder = random.sample(allPartners, k = len(allPartners))
+    partnerOrder = {key:value for key,value in enumerate(randPartnerOrder)}
+
+    return [dancesByPartner, danceOrder, partnerOrder]
+
+def buildHeat(dancesByPartner: dict):    
+    #perhaps map a random value for the keys to not create the same Heat each time
+    pass
 
 if __name__ == "__main__":
-    buildDict("2021-10 Program.xlsx")
+    dancesByPartner, danceOrder, partnerOrder = buildDatabases("2021-10 Program.xlsx")
+    
